609d3be7402fb67c49b052b4db525221d2536e2e
Jurgen Beliën <info@jurgenbelien.nl>
2020-04-03 15:01:43 +0200

    feat: support setting of led that won't be restored

--- src/IlluminatedButton.cpp
+++ src/IlluminatedButton.cpp
@@ -5,7 +5,7 @@ void IlluminatedButton::init() {
   for (uint8_t i = 0; i < leds.size(); i++) {
     Led& led = leds[i];
     led.init();
-    setLedIntensity(i, LED_OFF);
+    set(i, LED_OFF);
   }
 }
 
@@ -22,7 +22,7 @@ bool IlluminatedButton::pressed(const vector<uint8_t>& values, bool leading) {
 bool IlluminatedButton::held(unsigned int duration, const vector<uint8_t>& values, bool leading) {
     bool heldLeading = Button::held(duration, true);
   if (heldLeading) {
-    setLedIntensity(values, false);
+    setVolatile(values);
   }
   return leading ? heldLeading : Button::held(duration, leading);
 }
@@ -31,31 +31,35 @@ int IlluminatedButton::held() {
   return Button::held();
 }
 
-// Set LED and save state
 void IlluminatedButton::set(const vector<uint8_t>& values) {
-  setLedIntensity(values);
+  setLedIntensities(values, true);
 }
 void IlluminatedButton::set(uint8_t ledIndex, uint8_t value) {
-  setLedIntensity(ledIndex, value);
+  setLedIntensity(ledIndex, value, true);
+}
+
+void IlluminatedButton::setVolatile(const vector<uint8_t>& values) {
+  setLedIntensities(values, false);
+}
+void IlluminatedButton::setVolatile(uint8_t ledIndex, uint8_t value) {
+  setLedIntensity(ledIndex, value, false);
+}
+
+void IlluminatedButton::restore() {
+  IlluminatedButton::setLedIntensities(intensities, false);
 }
 
 // Private methods
-void IlluminatedButton::setLedIntensity(const vector<uint8_t>& values, bool save) {
+void IlluminatedButton::setLedIntensities(const vector<uint8_t>& values, bool save) {
   for (uint8_t i = 0; i < values.size() && i < leds.size(); i++) {
     setLedIntensity(i, values[i], save);
   }
 }
+
 void IlluminatedButton::setLedIntensity(uint8_t ledIndex, uint8_t value, bool save) {
   leds[ledIndex].set(value);
-
   if (save) {
     intensities[ledIndex] = value;
   }
 }
-void IlluminatedButton::restore() {
-  for (uint8_t i = 0; i < leds.size() && i < intensities.size(); i++) {
-    Led& led = leds[i];
-    uint8_t intensity = intensities[i];
-    led.set(intensity);
-  }
-}
+
